41b68e7bc806abc61368922bb690713bf300dd2e
alamdan-nartis <avidan@nartis.ai>
2021-11-17 17:33:30 +0200

    updated with game

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,7 @@
 //1.
 import React from 'react';
 import Amplify from 'aws-amplify';
+import Game from './tic-tac-toe';
 //2.
 import awsExports from './aws-exports';
 //3.
@@ -11,7 +12,8 @@ function Dashboard() {
  return(
     <div>
         <AmplifySignOut/> //5.
-        <h1>This is your logged in dashboard.</h1>
+        <Game />,
+        <h1>This is your logged in dashboard app.</h1>
     </div>
  )
 }
